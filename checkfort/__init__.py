dc9c5c6e3903a584cf909c18ace900c2c35c3f30
Shawn Chin <shawnchin@gmail.com>
2013-03-04 15:13:17 +0000

    changed homepage to github project

--- checkfort/__init__.py
+++ checkfort/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "0.1.0"
 __author__ = "Shawn Chin <shawn.chin@stfc.ac.uk>"
-__url__ = "https://pypi.python.org/pypi/checkfort"
+__url__ = "https://github.com/shawnchin/checkfort"
 
 project_url = __url__
